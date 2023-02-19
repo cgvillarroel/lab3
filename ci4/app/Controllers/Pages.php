925142e82c86c2dfab19ad10f15870c2811d4a9e
cgvillarroel <cgvillarroel@student.apc.edu.ph>
2023-02-19 13:48:53 +0800

    chore: formatting

--- ci4/app/Controllers/Pages.php
+++ ci4/app/Controllers/Pages.php
@@ -6,22 +6,22 @@ use CodeIgniter\Exceptions\PageNotFoundException; // Add this line
 
 class Pages extends BaseController
 {
-    public function index()
-    {
-        return view('welcome_message');
+  public function index()
+  {
+    return view('welcome_message');
+  }
+
+  public function view($page = 'home')
+  {
+    if (!is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+      // Whoops, we don't have a page for that!
+      throw new PageNotFoundException($page);
     }
 
-    public function view($page = 'home')
-    {
-        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
-            // Whoops, we don't have a page for that!
-            throw new PageNotFoundException($page);
-        }
+    $data['title'] = ucfirst($page); // Capitalize the first letter
 
-        $data['title'] = ucfirst($page); // Capitalize the first letter
-
-        return view('templates/header', $data)
-            . view('pages/' . $page)
-            . view('templates/footer');
-    }
+    return view('templates/header', $data)
+      . view('pages/' . $page)
+      . view('templates/footer');
+  }
 }
--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,8 +1,9 @@
 <!doctype html>
 <html>
-<head>
-    <title>CodeIgniter Tutorial</title>
-</head>
-<body>
 
-    <h1><?= esc($title) ?></h1>
+<head>
+  <title>CodeIgniter Tutorial</title>
+</head>
+
+<body>
+  <h1><?= esc($title) ?></h1>
